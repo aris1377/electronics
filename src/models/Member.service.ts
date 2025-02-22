6f43a3b19e91afa10c1d40fd4fb81c3681214b05
aris1377 <bek77519101@gmail.com>
2025-02-22 14:01:43 +0900

    Feat: develop byctptJs

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,6 +2,7 @@ import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
   private readonly memberModel;
@@ -15,7 +16,12 @@ class MemberService {
       .findOne({ memberType: MemberType.SHOP })
       .exec();
     console.log("exist", exist);
-    if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+      if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+      
+
+      
+      const salt = await bcrypt.genSalt();
+      input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
 
     try {
       const result = await this.memberModel.create(input);
@@ -33,9 +39,13 @@ class MemberService {
                 { memberNick: 1, memberPassword: 1}
             )
             .exec();
-        if(!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK)
-        const isMatch = input.memberPassword === member.memberPassword;
-        console.log("isMatch", isMatch);
+        if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK)
+        
+        const isMatch = await bcrypt.compare(
+            input.memberPassword,
+            member.memberPassword);
+        // const isMatch = input.memberPassword === member.memberPassword;
+        
 
         if (!isMatch) {
             throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD)

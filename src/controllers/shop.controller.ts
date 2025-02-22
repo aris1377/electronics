0bcb29c9ca9a3f0dd530030292bffc72c3d398d3
aris1377 <bek77519101@gmail.com>
2025-02-22 12:15:15 +0900

    Feat: develop signup process

--- src/controllers/shop.controller.ts
+++ src/controllers/shop.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
+import { MemberInput } from "../libs/types/member";
+import { MemberType } from "../libs/enums/member.enum";
 
 const shopController: T = {};
 shopController.goHome = (req: Request, res: Response) => {
@@ -39,12 +41,20 @@ shopController.processLogin = (req: Request, res: Response) => {
   }
 };
 
-shopController.processSignup = (req: Request, res: Response) => {
+shopController.processSignup = async (req: Request, res: Response) => {
   try {
     console.log("processSignup");
-    res.send("DONE");
+
+    const newMember: MemberInput = req.body;
+    newMember.memberType = MemberType.SHOP;
+
+    const memberService = new MemberService();
+    const result = await memberService.processSignup(newMember);
+
+    res.send(result);
   } catch (err) {
     console.error("Error, processSignup", err);
+    res.send(err);
   }
 };
 

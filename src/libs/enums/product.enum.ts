bdea9fada7401c6ad7e74632205398633ac64a2d
aris1377 <bek77519101@gmail.com>
2025-04-07 12:12:16 +0900

    feat: integrate product enums

---
+++ src/libs/enums/product.enum.ts
@@ -0,0 +1,80 @@
+//Toys
+export enum ToyGender {
+  GIRLS = "girls",
+  BOYS = "boys",
+}
+export enum ToyAgeRange {
+  AGE_0_3 = "0-3 years",
+  AGE_3_5 = "3-5 years",
+  AGE_6_7 = "6-7 years",
+}
+export enum ToySize {
+  ML = "ML",
+  M = "M",
+  L = "L",
+  S = "S",
+}
+export enum ToyColor {
+  RED = "red",
+  BLUE = "blue",
+  SKY_BLUE = "sky-blue",
+  ORANGE = "orange",
+}
+//Kids Clothing
+export enum ClothingGender {
+  GIRLS = "girls",
+  BOYS = "boys",
+}
+export enum ClothingAgeRange {
+  AGE_0_3 = "0-3 years",
+  AGE_3_5 = "3-5 years",
+  AGE_6_7 = "6-7 years",
+}
+export enum ClothingSize {
+  XS = "XS",
+  S = "S",
+  M = "M",
+  L = "L",
+  XL = "XL",
+}
+export enum ClothingColorForBoys {
+  BLUE = "blue",
+  GRAY = "gray",
+  GREEN = "green",
+  YELLOW = "yellow",
+  RED = "red",
+  ORANGE = "orange", 
+}
+export enum ClothingColorForGirls {
+  PINK = "pink",
+  WHITE = "white",
+  PURPLE = "purple",
+  GOLD = "gold",
+  PEACH = "peach"
+}
+// Kids Books
+ export enum BookCategory {
+   FAIRY_TALE = "Fairy Tale",
+   EDUCATIONAL = "Educational",
+   COLORING = "Coloring",
+   PUZZLE = "Puzzle",
+ }
+
+ export enum BookAgeRange {
+   AGE_0_3 = "0-3 years",
+   AGE_3_5 = "3-5 years",
+   AGE_6_7 = "6-7 years",
+   AGE_8_PLUS = "8+ years",
+ }
+
+ export enum BookLanguage {
+   UZBEK = "Uzbek",
+   RUSSIAN = "Russian",
+   ENGLISH = "English",
+   KOREAN = "Korean",
+ }
+
+ export enum BookCoverType {
+   HARD = "Hard Cover",
+   SOFT = "Soft Cover",
+ }
